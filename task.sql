064db90c6662b6f7dd0cd5eca40995f6c7445e4e
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-03-21 16:50:57 +0200

    fix the issues

--- task.sql
+++ task.sql
@@ -1,13 +1,18 @@
--- Create database and tables
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
+CREATE TABLE Countries(
+	ID INT,
+    Name VARCHAR(50),
+	City VARCHAR(50),
+    Street VARCHAR(50),
+    PRIMARY KEY (ID)
+);
 CREATE TABLE Warehouses (
     ID INT,
     Name VARCHAR(50),
-    Country VARCHAR(50), 
-	City VARCHAR(50),
-    Street VARCHAR(50),
+    LocationID INT, 
+    FOREIGN KEY (LocationID) REFERENCES Countries(ID) ON DELETE NO ACTION,
     PRIMARY KEY (ID)
 );
 
@@ -27,12 +32,15 @@ CREATE TABLE ProductInventory (
     PRIMARY KEY (ID)
 );
 
--- Populate test data
-
-INSERT INTO Warehouses (ID,Name,Country,City,Street)
-	VALUES (1,'Warehouse-1','Country1','City-1','Street-1');
-INSERT INTO Warehouses (ID,Name,Country,City,Street)
-	VALUES (2,'Warehouse-2','Country2','City-2','Street-2');
+INSERT INTO Countries (ID,Name,City,Street)
+	VALUES (1,'Country1','City-1','Street-1');
+INSERT INTO Countries (ID,Name,City,Street)
+	VALUES (2,'Country2','City-2','Street-2');
+    
+INSERT INTO Warehouses (ID,Name,LocationID)
+	VALUES (1,'Warehouse-1',1);
+INSERT INTO Warehouses (ID,Name,LocationID)
+	VALUES (2,'Warehouse-2',2);
 
 INSERT INTO Products(ID,Name)
 	VALUES (1, 'AwersomeProduct');
